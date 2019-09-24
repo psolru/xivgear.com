69ee5887c33516ce30342535d36d9f799aff8190
Patrick Scholz <paddl.scholz@gmail.com>
2019-09-24 23:34:41 +0200

    delete old asset folders

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-function _(s) { return document.getElementById(s); }
-
-function he(str) {
-	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
